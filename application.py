131d3744c594ee1557698370015b4aa926a06797
Mohamed Fesal <mohamedfesal99@gmail.com>
2021-11-23 23:54:29 +0200

    Update application.py

--- application.py
+++ application.py
@@ -40,4 +40,4 @@ def editStockItem():
 # Run App
 #=========================
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
