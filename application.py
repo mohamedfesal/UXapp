5bf1df58a5a7ea806e25bee63dbefbb8860e575e
Mohamed Fesal <mohamedfesal99@gmail.com>
2021-11-24 01:10:42 +0200

    Update application.py

--- application.py
+++ application.py
@@ -3,41 +3,41 @@
 # Import Flask Framwork
 #=========================
 from flask import Flask, render_template
-app = Flask(__name__)
+UXCentersApp = Flask(__name__)
 
 #=========================
 # App Routes
 #=========================
 
-@app.route("/")
+@UXCentersApp.route("/")
 def home():
     return render_template("index.html", title="Home", companyName="UX Centers")
 
-@app.route("/dashboard")
+@UXCentersApp.route("/dashboard")
 def bashboard():
     return render_template("/dashboard.html", title="Dashboard")
 
-@app.route("/wfhsheet")
+@UXCentersApp.route("/wfhsheet")
 def wfhsheet():
     return render_template("/wfhsheet.html", title="WFH Sheet") 
 
-@app.route("/returned")
+@UXCentersApp.route("/returned")
 def returned():
     return render_template("/returned.html", title="Returned PCs")
     
-@app.route("/pcdetails")
+@UXCentersApp.route("/pcdetails")
 def pcdetails():
     return render_template("/pcdetails.html", title="PCs Details")
 
-@app.route("/stock")
+@UXCentersApp.route("/stock")
 def stock():
     return render_template("/stock.html", title="Stock")
 
-@app.route("/stock/edit-stock-item")
+@UXCentersApp.route("/stock/edit-stock-item")
 def editStockItem():
     return render_template("/edit-stock-item.html", title="Edit Stock Item")
 #=========================
 # Run App
 #=========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    UXCentersApp.run(debug=True)
